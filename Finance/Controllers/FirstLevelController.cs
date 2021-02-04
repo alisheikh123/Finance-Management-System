15f17cca85ef366aab43765a2af8cd45dfc42af2
Ali Sheikh <MISD_uskt@outlook.com>
2021-02-04 16:15:14 +0500

    Add project files.

---
+++ Finance/Controllers/FirstLevelController.cs
@@ -0,0 +1,101 @@
+﻿using Finance.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+ 
+ 
+
+namespace Finance.Controllers
+{
+    public class FirstLevelController : Controller
+    {
+
+        dbFinanceEntities dbEntities = new dbFinanceEntities();
+        // GET: ChartOfAccount
+        [HttpGet]
+        public ActionResult FirstLevel()
+        {
+            tblFirstLevel fruit = new tblFirstLevel();
+            fruit.FirstLevelDetails = dbEntities.tblFirstLevels.ToList();
+
+            return View(fruit);
+        }
+
+
+
+        [HttpGet]
+        // GET: FirstLevel
+        public ActionResult FirstLevelEdit(int id = 0)
+        {
+
+            tblFirstLevel tblFirstLevel = dbEntities.tblFirstLevels.Find(id);
+            tblFirstLevel.FirstLevelDetails = dbEntities.tblFirstLevels.ToList();
+            return View("FirstLevel", tblFirstLevel);
+        }
+
+
+        [HttpPost]
+        public ActionResult FirstLevel(tblFirstLevel firstLevelModel)
+        {
+            //if (dbEntities.tblFirstLevels.Any(x => x.firstLevelAccount == firstLevelModel.firstLevelAccount) && firstLevelModel.firstLevelId <= 0)
+            //{
+            //    ViewBag.DuplicateMsg = "Account name already exist!";
+            //    firstLevelModel.FirstLevelDetails = dbEntities.tblFirstLevels.ToList();
+            //    return View("FirstLevel", firstLevelModel);
+            //}
+            //else
+            if (dbEntities.tblFirstLevels.Any(x => x.firstLevelAccount == firstLevelModel.firstLevelAccount && x.firstLevelId != firstLevelModel.firstLevelId))
+            {
+                ViewBag.DuplicateMsg = "Account name already exist!";
+                firstLevelModel.FirstLevelDetails = dbEntities.tblFirstLevels.ToList();
+                return View("FirstLevel", firstLevelModel);
+            }
+            else if (firstLevelModel.firstLevelId > 0)
+            {
+                dbEntities.Entry(firstLevelModel).State = EntityState.Modified;
+                dbEntities.SaveChanges();
+            }
+            else
+            {
+                dbEntities.tblFirstLevels.Add(firstLevelModel);
+                dbEntities.SaveChanges();
+            }
+
+            ModelState.Clear();
+            ViewBag.SuccessMsg = "Save successfully!";
+
+            tblFirstLevel fruit = new tblFirstLevel();
+            fruit.FirstLevelDetails = dbEntities.tblFirstLevels.ToList();
+
+            //FirstLevel();
+            return View("FirstLevel", fruit);
+        }
+
+
+
+        [HttpGet]
+        // GET: FirstLevel
+        public ActionResult FirstLevelDelete(int id = 0)
+        {
+            tblFirstLevel tblFirstLevel = dbEntities.tblFirstLevels.Find(id);
+            dbEntities.tblFirstLevels.Remove(tblFirstLevel);
+            dbEntities.SaveChanges();
+
+            ModelState.Clear();
+            ViewBag.SuccessMsg = "Save successfully!";
+
+            tblFirstLevel fruit = new tblFirstLevel();
+            fruit.FirstLevelDetails = dbEntities.tblFirstLevels.ToList();
+
+            return View("FirstLevel", fruit);
+
+        }
+
+
+
+
+    }
+}
---
+++ Finance/Controllers/SecondLevelController.cs
@@ -0,0 +1,127 @@
+﻿using Finance.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Finance.Controllers
+{
+    public class SecondLevelController : Controller
+    {
+        dbFinanceEntities dbEntities = new dbFinanceEntities();
+        vSecondLevel fruit = new vSecondLevel();
+
+        // GET: ChartOfAccount
+        [HttpGet]
+        public ActionResult SecondLevel()
+        {
+            //   tblSecondLevel sec = new tblSecondLevel();
+            //  sec.SecLevelDetails = dbEntities.tblSecondLevels.ToList();
+             fruit.SecLevelDetails = dbEntities.vSecondLevels.ToList();
+
+            List<tblFirstLevel> deptlist = dbEntities.tblFirstLevels.ToList();
+
+
+            ViewBag.deptlist = new SelectList(deptlist, "firstLevelId", "firstLevelAccount");
+            return View(fruit);
+        }
+        [HttpPost]
+        public ActionResult SecondLevel(vSecondLevel second)
+        {
+            if (dbEntities.tblSecondLevels.Any(x => x.secondLevelAccount == second.secondLevelAccount && x.firstLevelId==second.firstLevelId))
+            {
+                ViewBag.DuplicateMsg = "Account name already exist!";
+                second.SecLevelDetails = dbEntities.vSecondLevels.ToList();
+                second.SecLevelDetails = dbEntities.vSecondLevels.ToList();
+
+                List<tblFirstLevel> deptlists = dbEntities.tblFirstLevels.ToList();
+
+
+                ViewBag.deptlist = new SelectList(deptlists, "firstLevelId", "firstLevelAccount");
+
+
+                return View("SecondLevel", second);
+            }
+            else if (second.secondLevelId > 0)
+            {
+                
+                tblSecondLevel s = new tblSecondLevel();
+                s.firstLevelId = second.firstLevelId;
+                s.secondLevelId = second.secondLevelId;
+                s.secondLevelAccount = second.secondLevelAccount;
+                s.secondLevelDesc = second.secondLevelDesc;
+
+                dbEntities.Entry(s).State = EntityState.Modified;
+                dbEntities.SaveChanges();
+            }
+            else
+            {
+                tblSecondLevel s = new tblSecondLevel();
+                s.firstLevelId = second.firstLevelId;
+                s.secondLevelId = second.secondLevelId;
+                s.secondLevelAccount = second.secondLevelAccount;
+                s.secondLevelDesc = second.secondLevelDesc;
+
+                dbEntities.tblSecondLevels.Add(s);
+                dbEntities.SaveChanges();
+            }
+
+            ModelState.Clear();
+            ViewBag.SuccessMsg = "Save successfully!";
+            vSecondLevel fruit = new vSecondLevel();
+            fruit.SecLevelDetails = dbEntities.vSecondLevels.ToList();
+
+            List<tblFirstLevel> deptlist = dbEntities.tblFirstLevels.ToList();
+
+
+            ViewBag.deptlist = new SelectList(deptlist, "firstLevelId", "firstLevelAccount");
+            return View("SecondLevel", fruit);
+        }
+        [HttpGet]
+        public ActionResult SecondLevelEdit(int id = 0)
+        {
+           
+
+            vSecondLevel secondLevel = new vSecondLevel();  
+            secondLevel.SecLevelDetails = dbEntities.vSecondLevels.Where(x => x.secondLevelId == id).ToList();
+       
+
+            foreach (var item in secondLevel.SecLevelDetails)
+            {
+                secondLevel.secondLevelAccount = item.secondLevelAccount;
+                secondLevel.secondLevelDesc = item.secondLevelDesc;
+                secondLevel.secondLevelId = id;
+                secondLevel.firstLevelId = item.firstLevelId; 
+            }
+
+
+            List<tblFirstLevel> deptlist = dbEntities.tblFirstLevels.ToList();
+ViewBag.deptlist = new SelectList(deptlist, "firstLevelId", "firstLevelAccount");
+            return View("SecondLevel", secondLevel);
+        }
+
+        [HttpGet]
+        // GET: FirstLevel
+        public ActionResult SecondLevelDelete(int id = 0)
+        {
+            tblSecondLevel tblsecLevel = dbEntities.tblSecondLevels.Find(id);
+           
+            dbEntities.tblSecondLevels.Remove(tblsecLevel);
+            dbEntities.SaveChanges();
+
+            ModelState.Clear();
+            ViewBag.SuccessMsg = "Save successfully!";
+            fruit.SecLevelDetails = dbEntities.vSecondLevels.ToList();
+
+            List<tblFirstLevel> deptlist = dbEntities.tblFirstLevels.ToList();
+
+
+            ViewBag.deptlist = new SelectList(deptlist, "firstLevelId", "firstLevelAccount");
+            return View("SecondLevel", fruit);
+        
+
+        }
+    }
+}
